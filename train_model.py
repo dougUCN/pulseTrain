967b15b9d48cc5d4e9f16c92945df0d33b2894d0
dougUCN <douglas.kt.wong@gmail.com>
2023-07-23 20:59:26 -0600

    Add 1D Resnet from Hong et al 2020

--- train_model.py
+++ train_model.py
@@ -9,6 +9,16 @@ import pandas as pd
 from tqdm import tqdm
 from src.dataset import pulse_train_dataset
 
+# Parameters
+LOADER_PARAMS = {
+    "batch_size": 64,
+    "shuffle": True,
+    "num_workers": 4,
+    "pin_memory": True,
+}
+MAX_EPOCHS = 25
+LEARNING_RATE = 0.01
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -19,14 +29,10 @@ def main():
         "-f",
         "--filename",
         type=str,
-        required=True,
+        default="in/metadata.csv",
         help="metadata filename",
     )
     args = parser.parse_args()
-
-    # Parameters
-    params = {"batch_size": 64, "shuffle": True, "num_workers": 4, "pin_memory": True}
-    max_epochs = 50
 
     # Parse metadata file and set up data loading
     dataset = {}
@@ -36,22 +42,47 @@ def main():
         metadata["name"].tolist(), metadata["data_file"].tolist()
     ):
         dataset[name] = pulse_train_dataset(data_file, args.filename)
-        dataset_generator = torch.utils.data.DataLoader(dataset[name], **params)
+        dataset_generator[name] = torch.utils.data.DataLoader(
+            dataset[name], **LOADER_PARAMS
+        )
 
     # CUDA for PyTorch
     use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda:0" if use_cuda else "cpu")
+    device = torch.device("cuda" if use_cuda else "cpu")
     torch.backends.cudnn.benchmark = True
 
-    # Loop over epochs
-    for epoch in tqdm(range(max_epochs)):
-        # Train
+    # Initialize model
+
+    # Optimizer
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
+
+    # Print general info
+    print(f"device: {device}")
+    print(LOADER_PARAMS)
+    print(f"learning rate: {LEARNING_RATE}")
+    print(f"Training set has {len(dataset_generator['training'])} instances")
+    print(f"Validation set has {len(dataset_generator['validation'])} instances")
+
+    # TRAINING LOOP
+    for epoch in tqdm(range(MAX_EPOCHS)):
         for local_batch, local_labels in dataset_generator["training"]:
             # Transfer to GPU
             local_batch, local_labels = local_batch.to(device), local_labels.to(device)
 
-            # Model computations
-            # [...]
+            # Zero gradients for every batch
+            optimizer.zero_grad()
+
+            # Forward pass.
+
+            # Calculate the loss and accuracy
+
+            # Backpropagation
+
+            # Update the weights.
+            optimizer.step()
+
+        # Calculate loss and accuracy for the complete epoch.
 
         # Validation
         with torch.set_grad_enabled(False):
@@ -63,6 +94,10 @@ def main():
 
                 # Model computations
                 # [...]
+
+    # END TRAINING LOOP
+
+    # TODO Save model
 
     return
 
